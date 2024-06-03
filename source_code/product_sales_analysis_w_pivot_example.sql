572d1c4fee41c869b4aee0a1e6dc834f8136319f
PiotrBelniak <piotrbelniak@gmail.com>
2024-06-03 15:35:41 +0200

    Update product_sales_analysis_w_pivot_example.sql

--- source_code/product_sales_analysis_w_pivot_example.sql
+++ source_code/product_sales_analysis_w_pivot_example.sql
@@ -14,44 +14,74 @@ AS
 yearly_product_sales(product_name,year, price,sales_value,sales_volume, transaction_cnt)
 AS
 (
-select product_name,year, ANY_VALUE(price),SUM(sales_value) as yearly_sales_value, SUM(sales_volume) as yearly_sales_amt, SUM(transaction_cnt) as transaction_cnt
+select product_name
+        ,year
+        , ANY_VALUE(price)
+        ,SUM(sales_value) as yearly_sales_value
+        , SUM(sales_volume) as yearly_sales_amt
+        , SUM(transaction_cnt) as transaction_cnt
 from basis
-group by product_name,year)
-,
+group by product_name,year
+),
 product_bucket_assignment(year,product_name,bucket_nr)
 as
 (
-select year,product_name, NTILE(5) OVER (PARTITION BY year ORDER BY sales_value desc)
-from yearly_product_sales)
-,
+select year
+        ,product_name
+        , NTILE(5) OVER (PARTITION BY year ORDER BY sales_value desc)
+from yearly_product_sales
+),
 bucket_min_max_sales_value(year,bucket_nr,minimum_sales_val,maximum_sales_val, least_expensive_product, most_expensive_product)
 as
 (
-select year, bucket_nr,MIN(sales_value) as minimum_val, MAX(sales_value) as maximum_val, MIN(price) as least_expensive_product, MAX(price) as most_expensive_product
+select year
+        , bucket_nr
+        ,MIN(sales_value) as minimum_val
+        , MAX(sales_value) as maximum_val
+        , MIN(price) as least_expensive_product
+        , MAX(price) as most_expensive_product
 from yearly_product_sales JOIN product_bucket_assignment USING(product_name,year)
-group by year, bucket_nr)
-,
+group by year, bucket_nr
+),
 bucket_avg_monthly_transactions(year,bucket_nr,avg_monthly_transaction_cnt)
 as
 (
-select distinct year,bucket_nr, ROUND(AVG(SUM(transaction_cnt)) OVER (PARTITION BY year,bucket_nr),3)
+select distinct year
+        ,bucket_nr
+        , ROUND(AVG(SUM(transaction_cnt)) OVER (PARTITION BY year,bucket_nr),3)
 from basis JOIN product_bucket_assignment USING(product_name,year)
 group by year,bucket_nr, month
-)
-,bucket_avg_sales_diff_against_most_least_expensive(year,bucket_nr,most_expensive_avg_diff, least_expensive_avg_diff)
+),
+bucket_avg_sales_diff_against_most_least_expensive(year,bucket_nr,most_expensive_avg_diff, least_expensive_avg_diff)
 as
 (
-select year, bucket_nr, most_expensive_sales_volume-avg_sales_volume roznica_najdrozszy_srednia,least_expensive_sales_volume-avg_sales_volume roznica_najtanszy_srednia
+select year
+        , bucket_nr
+        , most_expensive_sales_volume-avg_sales_volume most_expensive_avg_diff
+        , least_expensive_sales_volume-avg_sales_volume least_expensive_avg_diff
 from
 (
-select year, bucket_nr, ROUND(AVG(sales_volume)) avg_sales_volume, MAX(sales_volume) KEEP (DENSE_RANK FIRST ORDER BY price desc) most_expensive_sales_volume, MAX(sales_volume) KEEP (DENSE_RANK FIRST ORDER BY price asc) least_expensive_sales_volume
+select year
+        , bucket_nr
+        , ROUND(AVG(sales_volume)) avg_sales_volume
+        , MAX(sales_volume) KEEP (DENSE_RANK FIRST ORDER BY price desc) most_expensive_sales_volume
+        , MAX(sales_volume) KEEP (DENSE_RANK FIRST ORDER BY price asc) least_expensive_sales_volume
 from yearly_product_sales JOIN product_bucket_assignment USING(product_name,year)
 group by year, bucket_nr
 ))
 
-select year,bucket_nr,minimum_sales_val,maximum_sales_val, least_expensive_product, most_expensive_product,avg_monthly_transaction_cnt,most_expensive_avg_diff, least_expensive_avg_diff
+select year
+        ,bucket_nr
+        ,minimum_sales_val
+        ,maximum_sales_val
+        , least_expensive_product
+        , most_expensive_product
+        ,avg_monthly_transaction_cnt
+        ,most_expensive_avg_diff
+        , least_expensive_avg_diff
 from
-    bucket_min_max_sales_value JOIN bucket_avg_monthly_transactions USING(year,bucket_nr) 
+    bucket_min_max_sales_value 
+    JOIN bucket_avg_monthly_transactions USING(year,bucket_nr) 
     JOIN bucket_avg_sales_diff_against_most_least_expensive USING(year,bucket_nr) ;
 
 /*pivoting part of analysis*/
