6ea92c0bf9cbe3fd670e4b2f8cdf5c5135d95bea
PiotrBelniak <piotrbelniak@gmail.com>
2024-06-04 09:08:33 +0200

    corrections of code

--- source_code/differences_of_sales_across_different_time_periods.sql
+++ source_code/differences_of_sales_across_different_time_periods.sql
@@ -1,6 +1,6 @@
 select p.name
-        , c.year as rok
-        ,SUBSTR(c.quarter_id,5,1) as kwartal
+        , c.year as year
+        ,SUBSTR(c.quarter_id,5,1) as quarter
         ,SUM(s.quantity*p.price) as monthly_sum
         , ROUND(NVL((SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.quarter_id,5,1) ORDER BY c.year))/LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.quarter_id,5,1) ORDER BY c.year),0),4)*100 as yty_percent_diff
         , NVL(SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.quarter_id,5,1) ORDER BY c.year),0) as yty_diff
@@ -9,29 +9,29 @@ select p.name
 from sales s, product p, calendar c
 where s.prodid=p.id AND c.data=s.salesdate
 group by p.name, c.year, SUBSTR(c.quarter_id,5,1)
-order by p.name, kwartal, rok;
+order by p.name, quarter, year;
 
 
 select p.name
-        , c.year as rok
-        ,SUBSTR(c.month_id,5,2) as miesiac
+        , c.year as year
+        ,SUBSTR(c.month_id,5,2) as month
         ,SUM(s.quantity*p.price) as monthly_sum
         , ROUND(NVL((SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year))/LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year),0),4)*100 as yty_percent_diff
         , NVL(SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year),0) as yty_diff
-        , ROUND(NVL((SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)))/LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)),0),4)*100 as qtq_percent_diff
-        , NVL(SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)),0) as qtq_diff
+        , ROUND(NVL((SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)))/LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)),0),4)*100 as mtm_percent_diff
+        , NVL(SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,c.year ORDER BY SUBSTR(c.month_id,5,2)),0) as mtm_diff
 from sales s, product p, calendar c
 where s.prodid=p.id AND c.data=s.salesdate
 group by p.name, c.year, SUBSTR(c.month_id,5,2)
-order by p.name, miesiac, rok;
+order by p.name, month, year;
 
 select * from
 (
-select product_name, rok, miesiac,yty_percent_diff, RANK() OVER (PARTITION BY product_name,miesiac ORDER BY yty_percent_diff desc) as ranking from
+select product_name, year, month,yty_percent_diff, RANK() OVER (PARTITION BY product_name,month ORDER BY yty_percent_diff desc) as ranking from
 (   
     select p.name as product_name
-        , c.year as rok
-        ,SUBSTR(c.month_id,5,2) as miesiac
+        , c.year as year
+        ,SUBSTR(c.month_id,5,2) as month
         ,SUM(s.quantity*p.price) as monthly_sum
         , ROUND(NVL((SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year))/LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year),0),4)*100 as yty_percent_diff
         , NVL(SUM(s.quantity*p.price)-LAG(SUM(s.quantity*p.price),1) OVER (PARTITION BY p.name,SUBSTR(c.month_id,5,2) ORDER BY c.year),0) as yty_diff
@@ -40,7 +40,7 @@ select product_name, rok, miesiac,yty_percent_diff, RANK() OVER (PARTITION BY pr
     from sales s, product p, calendar c
     where s.prodid=p.id AND c.data=s.salesdate
     group by p.name, c.year, SUBSTR(c.month_id,5,2)
-    order by p.name, miesiac, rok
+    order by p.name, month, year
 )
 )
 where ranking<=2;
